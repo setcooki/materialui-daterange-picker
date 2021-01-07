8f1ddb85b9c38c556b7d896ef00c6916179f6770
setcookie <set@cooki.me>
2021-01-07 08:14:45 +0100

    adding dist

--- package/dist/components/DateRangePickerExporter.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { DateRangePickerWrapperProps } from './DateRangePickerWrapper';
-declare const DateRangePickerExporter: React.FunctionComponent<DateRangePickerWrapperProps>;
-export default DateRangePickerExporter;
--- package/dist/generateClassName.d.ts
+++
@@ -1,1 +0,0 @@
-export default function generateClassName(rule: any, sheet: any): string;
